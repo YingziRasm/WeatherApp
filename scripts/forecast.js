b9e7e0d19b683b69254b7a38b45c2c62ad8f74ff
YingziRasm <87018291+YingziRasm@users.noreply.github.com>
2021-07-14 10:01:22 +0200

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,28 @@
+//all of JS that would be responsiable for interacting with the weather api and getting data//
+const key= "dyB467ODp3SeIdAvkqgwODTc3H0AxBWy";
+// get weather information
+const getWeather =async (id) =>{
+    const base="http://dataservice.accuweather.com/currentconditions/v1/"
+    const query= `${id}?apikey=${key}`;
+
+    const response= await fetch(base+query);
+    const data= await response.json();
+    return data[0];
+}
+
+// get city information
+const getCity = async (city)=> {
+    const base ="http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query =`?apikey=${key}&q=${city}`;
+    const response = await fetch (base + query);
+    const data = await response.json();
+
+   return data[0];
+}
+
+   /* getCity ("oslo").then(data=>{
+        return getWeather(data.Key);
+    }).then(data=>{
+        console.log(data);
+    }).catch (err => console.log(err));*/
+
